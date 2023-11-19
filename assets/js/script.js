fa0fb83312ba4b151dc5e56cdaee0967b4b24db2
BenPalac1 <ben.palacpac@gmail.com>
2023-11-19 12:17:38 -0500

    updated java script, added questions and answer array

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// array of questions and answers
+
+var questions = [
+  {
+      title: 'Commonly used data types do NOT include:',
+      options: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+  },
+
+  {
+      title: 'DOM stands for _____.',
+      options: ['Download Or Modify', 'Duplicate Or Match', 'Document Object Model', 'Defrag Over Manipulate'],
+      answer: 'Document Object Model',
+  },
+
+  {
+      title: 'A common coding language used for styling is _____.',
+      options: ['XYZebra', 'Picture Perfect', 'CSS', 'StylesRuS'],
+      answer: 'CSS',
+  },
+
+  {
+      title: 'A common application used to traverse a computer and its files is a _____.',
+      options: ['Slack', 'Terminal', 'Task Manager', 'Menu'],
+      answer: 'Terminal',
+  },
+
+  {
+      title: 'API stands for _____.',
+      options: ['Actual Percentage Incurred', 'Augmented Post Intervals', 'It does not stand for anything', 'Application Programming Interface'],
+      answer: 'Application Programming Interface',
+  },
+
+  {
+      title: 'BONUS QUESTION!! Who would win in a fight, Lemmy or God?',
+      options: ['Lemmy', 'God', 'Neither', 'Trick question, Lemmy is God'],
+      answer: 'Trick question, Lemmy is God',
+  },
+];
